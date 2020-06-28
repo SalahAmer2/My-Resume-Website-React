84f49cebdaf0bf6e54b42abe59463bf6773027ed
SalahAmer2 <salahayman2@gmail.com>
2020-06-28 06:57:06 +0200

    put in styles of navbar component

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,39 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import MenuIcon from '@material-ui/icons/Menu';
 import Avatar from '@material-ui/core/Avatar';
+
+import './navbar.styles.scss';
 
 class NavBar extends React.Component {
     constructor() {
         super();
 
-        this.state = {
-            iconIsOpen: false
-        }
+        // this.state = {
+        //     iconIsOpen: false
+        // }
     }
 
-    handleClick = () => {
-        this.setState(prevState => ({
-            iconIsOpen: !prevState.iconIsOpen
-        }))
-    }
-//${this.state.iconIsOpen ? 'change': null}
+    // handleClick = () => {
+    //     this.setState(prevState => ({
+    //         iconIsOpen: !prevState.iconIsOpen
+    //     }))
+    // }
+
     render() {
         return (
             <div className='header'>
-                <div className={`container ${this.state.iconIsOpen ? 'change' : null}`} onClick={this.handleClick}>
+                {/* <div className={`container ${this.state.iconIsOpen ? 'change' : null}`} onClick={this.handleClick}>
                     <div className="bar1"></div>
                     <div className="bar2"></div>
                     <div className="bar3"></div>
-                </div>
+                </div> */}
+                <MenuIcon className='toggle' />
                 <h4 style={{fontWeight:'normal'}} className="navBar">
-                    <Link className='logo-container' to="/">
+                    <Link className='navbarLink' to="/">
                         <Avatar src="/broken-image.jpg" />
                     </Link>
-                    <Link className='option' to='/my-projects'>
+                    <Link className='navbarLink' to='/my-projects'>
                         My Projects
                     </Link>
-                    <Link className='option' to='/my-certificates'>
+                    <Link className='navbarLink' to='/my-certificates'>
                         Certificates
                     </Link>
                 </h4>
